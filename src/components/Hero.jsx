5ae325459a354a870390840bee03c5378a6ca1a5
Mahim <mahimmasrafi04@gmail.com>
2024-11-04 13:19:30 +0600

    deploy fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@
 const Hero = ({scrollIntoMain}) => {
 
   return (
-    <div className='mt-11 w-full rounded-lg bg-[url(./assets/cover.png)] py-28 bg-center bg-cover flex flex-col justify-center items-center gap-6 font-lexend text-center'>
+    <div className='mt-11 w-full rounded-lg bg-[url(/assets/cover.png)] py-28 bg-center bg-cover flex flex-col justify-center items-center gap-6 font-lexend text-center'>
         <h1 className="max-w-[80%]  font-bold text-white text-6xl">Discover an exceptional cooking class tailored for you!</h1>
         <p className="max-w-[80%] text-white">Explore delicious recipes crafted for every taste and skill level. Discover ingredients, cooking times, and nutrition—all in one place. Start cooking something new today!</p>
         <div className="flex gap-6">
